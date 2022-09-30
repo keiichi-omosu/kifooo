b8fbe4e9c2e5dbb5561e22cd46962a98868edd96
kurubushionline <keiichi.omosu@luxiar.com>
2022-10-01 01:09:03 +0900

    将棋盤の初期表示を行なった

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { CsaParser} from './csa_parser';
+import { Parsers } from 'json-kifu-format'
+import { BoardPlayer } from './board_player.ts'
 
 class Square extends React.Component {
    render() {
        return (
            <button className="square">
-             {this.props.pos_x}
-             {this.props.pos_y}
+             {this.props.piece}
            </button>
        );
    }
 }
 
 class Board extends React.Component {
-  renderSquare(x, y) {
-    return <Square pos_x={x} pos_y={y} />;
+  constructor(props) {
+    super(props)
+    this.state = { board: props.board };
+  }
+
+  renderSquare(piece) {
+    return <Square piece={piece} />;
   }
 
   render() {
     let items = new Array<JSX.Element>();
-    for(let y = 0; y < 9; ++y) {
+    this.state.board.forEach((row) => {
       let columns = new Array<JSX.Element>();
-      for(let x = 0; x < 9; ++x) {
-        columns = columns.concat(this.renderSquare(x, y));
-      }
+      row.forEach((piece) =>  {
+        columns = columns.concat(this.renderSquare(piece));
+      });
       items = items.concat(<div className="board-row">{columns}</div>);
-    }
+    });
     return (<div>{items}</div>);
   }
 }
 
 class Game extends React.Component {
-   openKif(event) {
+   constructor(props) {
+     super(props)
+     this.state = { board_player: new BoardPlayer() };
+   }
+
+   openKif = (event) => {
      const reader = new FileReader()
      reader.onload = async (e) => {
        const text = (e.target.result)
-       CsaParser.parse(text)
+       const kif = Parsers.parseCSA(text);
+       // TypeScriptの連想配列型定義どうなるの？
+       const move = kif.moves[1]['move']
+       this.state.board_player.move(move['from']['x'], move['from']['y'], move['to']['x'], move['to']['y'], move['piece'])
+       this.setState({ board_player: this.state.board_player });
      }
      reader.readAsText(event.target.files[0])
    }
@@ -46,7 +60,7 @@ class Game extends React.Component {
            <div className="game">
                <div><input type='file' onChange={this.openKif} /></div>
                <div className="game-board">
-                   <Board />
+                   <Board board = { this.state.board_player.get_board() } />
                </div>
                <div className="game-info">
                    <div>{/* status */}</div>
